a2282dd2e609f2b7d68a889702beb4d50c43efa8
chipseater <jeanpauldemalet@yahoo.fr>
2022-11-28 11:14:42 +0000

    [Auth] implemented register

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,6 @@
-function fetchRoute(route, verb) {
+function fetchRoute(route, verb, data={}) {
   return new Promise((resolve, reject) => {
-    fetch(`http://localhost:5000${route}`, {
+    fetch(`https://5000-chipseater-nsiloginsyst-qsj521wbgao.ws-eu77.gitpod.io${route}`, {
       method: verb,
       headers: {
         'Content-Type': 'application/json',
@@ -8,6 +8,7 @@ function fetchRoute(route, verb) {
       'Access-Control-Allow-Origin': '*',
       mode: 'cors',
       cache: 'default',
+      body: JSON.stringify(data)
     })
       .then((res) => {
         res
--- js/register.js
+++ js/register.js
@@ -1,10 +1,52 @@
-function submitRegistration() {
-    console.log('Hello World')
-    fetchRoute('/register', 'POST')
+// To refactor
+function submitRegistration(event) {
+    event.preventDefault()
+
+    message_box = document.getElementById('message-box')
+
+    first_name = document.getElementById('first_name_input').value
+    last_name = document.getElementById('last_name_input').value
+    email = document.getElementById('email_input').value
+    passwd = document.getElementById('passwd_input').value
+    passwd_repeat = document.getElementById('passwd_repeat_input').value
+
+    if (passwd != passwd_repeat) {
+        console.error('Passwords do not match')
+        message_box.innerHTML = "Les mots de passe ne correspondent pas"
+        message_box.classList.toggle('red')
+        message_box.classList.remove('green')
+        return
+    }
+
+    if (first_name == '' || last_name == '' || email == '' || passwd == ''){
+        console.error('Some fields are not fullfilled')
+        message_box.innerHTML = "Veuillez remplir tout les champs"
+        message_box.classList.toggle('red')
+        message_box.classList.remove('green')
+        return
+    }
+
+    fetchRoute('/register', 'POST', {
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        passwd: passwd,
+    })
         .then(res => {
-            
+            console.log(res)
+
+            localStorage.setItem("access_token", JSON.stringify(res['access_token']))
+            localStorage.setItem("access_token", JSON.stringify(res['refresh_token']))
+
+            message_box.innerHTML = "Vous avez bien été enregistré"
+            message_box.classList.toggle('green')
+            message_box.classList.remove('red')
+
         })
         .catch(err => {
-
+            console.error(err)
+            message_box.innerHTML = "Une erreure est survenue"
+            message_box.classList.toggle('red')
+            message_box.classList.remove('green')
         })
 }
