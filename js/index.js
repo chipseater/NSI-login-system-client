7d380f3d374c83d1f92fa436200d540b27685393
chipseater <jeanpauldemalet@yahoo.fr>
2022-12-02 19:44:40 +0100

    [Todos] added date in cards

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 // Enable checkbox checking
-[...document.getElementsByClassName('checkbox')].forEach(checkbox => {
-    checkbox.addEventListener('click', (event) => {
-        if (event.target.innerHTML == 'check_box') {
-            event.target.innerHTML = 'check_box_outline_blank'
-        } else {
-            event.target.innerHTML = 'check_box'
-        }
-    })
+[...document.getElementsByClassName('checkbox')].forEach((checkbox) => {
+  checkbox.addEventListener('click', (event) => {
+    if (event.target.innerHTML == 'check_box') {
+      // Uncheck the box
+      event.target.innerHTML = 'check_box_outline_blank'
+    } else {
+      // Check the box
+      event.target.innerHTML = 'check_box'
+    }
+  })
 })
