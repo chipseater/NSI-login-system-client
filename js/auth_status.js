66c9e37ae6bbe254176151637bc3d504d970dcdf
chipseater <jeanpauldemalet@yahoo.fr>
2022-12-02 16:23:18 +0100

    [Todos] started prototyping UI

---
+++ js/auth_status.js
@@ -0,0 +1,35 @@
+not_logged_in_container = document.getElementById('not-logged-in')
+logged_in_container = document.getElementById('logged-in')
+
+token = localStorage.getItem('access_token')
+
+if (token && token != 'undefined') {
+  console.log(token)
+  not_logged_in_container.style.display = 'none'
+  logged_in_container.style.display = 'flex'
+} else {
+  not_logged_in_container.style.display = 'flex'
+  logged_in_container.style.display = 'none'
+}
+
+function logout() {
+  localStorage.setItem('access_token', '')
+  localStorage.setItem('refresh_token', '')
+
+  fetchRoute(
+    '/logout',
+    'POST',
+    false,
+    (data = {
+      refresh_token: localStorage.getItem('refresh_token'),
+    })
+  )
+    .then((_) => {
+      displaySuccess('Vous êtes bien déconnectés')
+    })
+    .catch((err) => console.error(err))
+
+  setTimeout(() => {
+    window.location.href = '/'
+  }, 500)
+}
--- js/index.js
+++ js/index.js
@@ -1,39 +1,10 @@
-not_logged_in_container = document.getElementById('not-logged-in')
-logged_in_container = document.getElementById('logged-in')
-
-token = localStorage.getItem('access_token')
-
-if (token && token != 'undefined') {
-    console.log(token)
-    not_logged_in_container.style.display = 'none'
-    logged_in_container.style.display = 'flex'
-} else {
-    not_logged_in_container.style.display = 'flex'
-    logged_in_container.style.display = 'none'
-}
-
-fetchRoute('/', 'GET', true)
-    .then(({ user_id }) => {
-        if (!user_id) {
-            document.getElementById('splash-text').innerHTML = 'Veuillez vous connecter pour accéder à votre contenu'
+// Enable checkbox checking
+[...document.getElementsByClassName('checkbox')].forEach(checkbox => {
+    checkbox.addEventListener('click', (event) => {
+        if (event.target.innerHTML == 'check_box') {
+            event.target.innerHTML = 'check_box_outline_blank'
         } else {
-            document.getElementById('splash-text').innerHTML = `Votre identifiant est ${user_id}`
+            event.target.innerHTML = 'check_box'
         }
     })
-
-function logout() {
-    localStorage.setItem('access_token', '')
-    localStorage.setItem('refresh_token', '')
-
-    fetchRoute('/logout', 'POST', false, data={
-        refresh_token: localStorage.getItem('refresh_token')
-    })
-        .then(_ => {
-            displaySuccess('Vous êtes bien déconnectés')
-        })
-        .catch(err => console.error(err))
-    
-    setTimeout(() => {
-        window.location.href = '/'
-    }, 500)
-}
+})
